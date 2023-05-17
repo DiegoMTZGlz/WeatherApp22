1d33f0e56639d1c3a70d6b768851425d211db94a
DiegoMTZGlz <a353198@uach.mx>
2023-05-16 20:11:16 -0600

    Se agregan los archivos de la práctica

---
+++ app/src/main/java/mx/uach/fing/WeatherApp22/HomeActivity.java
@@ -0,0 +1,35 @@
+package mx.uach.fing.WeatherApp22;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.logging.Logger;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        final Button btnCuu = findViewById(R.id.btnCuu);
+        btnCuu.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Logger.getLogger("app ->").info("presione un boton");
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.putExtra("CITY","Chihuahua");
+                startActivity(intent);
+            }
+        });
+    }
+    public void goToMty(View v){
+        Logger.getLogger("app ->").warning("peligro");
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        intent.putExtra("CITY","Monterrey");
+        startActivity(intent);
+    }
+}
